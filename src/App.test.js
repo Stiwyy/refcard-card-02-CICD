3dfdce467aa3fee89cb0a136ceccab29626b4eed
Stiwyy <andrinrenggli@outlook.com>
2026-05-08 16:54:10 +0200

    fix bug in code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App Ref. Card 02 link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/App Ref. Card 02/i);
   expect(linkElement).toBeInTheDocument();
 });
